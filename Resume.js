c5b80b122d17b8d8693e190175245bc9cef0ff99
aghataquee <taqueeagha05@gmail.com>
2024-10-06 23:27:59 +0530

    Resume

---
+++ Resume.js
@@ -0,0 +1,37 @@
+Taquee Agha
+ ✉️taqueeagha05@gmail.com 📞8533028219 📍Bangalore
+www.linkedin.com/in/taquee-agha-2420a4262
+PROFILE SUMMARY
+• Highly motivated and enthusiastic Full Stack Web Developer with 9-months of course from PW
+Skills.
+• Proficient in C++, HTML, CSS, JavaScript and React.js.
+• Strong knowledge with database technologies such as MySQL,MongoDB.
+• Knowledge of Data Structures and Algorithm.
+• Knowledge of version control systems such as GIT.
+• Excellent communication and teamwork abilities, ensuring effective collaboration with crossfunctional teams.
+• Looking For Entry Level Positions
+PRIMARY SKILLS
+• Languages: C++ and Python
+• Frameworks: MERN Stack
+• Frontend: HTML, CSS, JAVASCRIPT and REACT.Js
+• Databases: MySQL, MongoDB
+• IDE and Tools: Postman, Visual Studio Code
+PROJECTS :
+Social-Media App(https://github.com/aghataquee/SocialMedia.git)
+• Description:Developed a Social Media App using the MERN stack, offering a platform for users
+to connect, share content, and interact in real-time. The app features a responsive and intuitive 
+user interface built with React, and a robust backend powered by Node.js, Express, and
+MongoDB, ensuring secure user data management and efficient content delivery.
+• Client:User,Admin
+• Frontend:React.js
+• Backend:Nodejs,Express,Database:MongoDB
+Learning Management System(https://github.com/aghataquee/LMS.git)
+• Description: I developed a Learning Management System (LMS) using Node.js, Express, and
+MongoDB, with a Razorpay integration for secure payments. The system supports role-based
+access for admins, instructors, and students, allowing course creation, enrollment, and progress
+tracking. Payments are securely processed through Razorpay, unlocking course access upon
+successful transactions.
+EDUCATION
+B.Tech in Electrical Engineering AMU 2020-2024 7.2 CPI
+CLASS XII AMU 2019 69.4%
+CLASS X AMU 2017 9.6 CPI
